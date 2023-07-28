742b15b95dc4a2037e2dba1342ef1597d9740448
Dandara <122398596+dandadivclass@users.noreply.github.com>
2023-07-27 21:31:45 -0300

    Create função.java

---
+++ função.java
@@ -0,0 +1,10 @@
+public class FuncoesTiagoAguiarYoutubeAula {
+    public static void main(String[] args) {
+        soma(55, 63);
+        soma(14, 14);  // a função pode ser usada diversas vezes em diferentes lugares no código
+    }
+
+    static void soma(int x, int y) {  //função que espera duas variaveis do tipo inteiro como parâmetro 
+        System.out.println(x + y);
+    }
+}
